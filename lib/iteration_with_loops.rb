e98049c4726f2b2b4a47f3327fcbb3d856a17b1e
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-31 03:37:46 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,15 +4,16 @@ def find_min_in_nested_arrays(src)
   
 new_array = []
 row_index = 0
-while row_index < src.count do
-  element_index = 0 
+element_index = 0 
+while row_index < src[element_index].count do
+  new_array << src[row_index]
   while element_index < src[row_index].count do
-    new_array << src[row_index][element_index]
+    new_array << src[element_index]
     element_index += 1
+  end
   row_index += 1 
   end
 end
-
 
 
 def find_greater_pair
